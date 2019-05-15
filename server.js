ad99a2da70e543cd3ff4219d8d389099211f1433
Mauricio Miranda <maurimiranda@gmail.com>
2019-05-15 14:54:21 -0300

    Add gssurgo (soil) endpoint to server

--- server.js
+++ server.js
@@ -18,8 +18,18 @@ mapnik.register_default_input_plugins();
 
 const mercator = new SphericalMercator();
 
-// Prepare imagery config template
+// Setup imagery config template
 const imagery = Handlebars.compile(fs.readFileSync('imagery.xml', 'utf8'));
+
+// Setup gssurgo config
+const gssurgo = Handlebars.compile(fs.readFileSync('gssurgo.xml', 'utf8'))({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dbname: process.env.DB_NAME,
+  table: process.env.DB_TABLE
+});
 
 // Create Express App
 const app = express();
@@ -27,15 +37,15 @@ const app = express();
 // Add logger
 app.use(morgan('dev'));
 
-app.get('/:uuid/:z/:x/:y.png', function(req, res, next) {
+const generateTile = function(req, res, next, config) {
   // Create Mapnik object
   const map = new mapnik.Map(256, 256);
 
   // Convert XYZ to BoundingBox and set the Map extent
   map.zoomToBox(mercator.bbox(req.params.x, req.params.y, req.params.z, false, '900913'));
 
-  // Read map config from modified XML
-  map.fromString(imagery({ uuid: req.params.uuid}), function(err, map) {
+  // Read map config
+  map.fromString(config, function(err, map) {
     if (err) return next(err);
 
     // Create image object from map config
@@ -51,6 +61,16 @@ app.get('/:uuid/:z/:x/:y.png', function(req, res, next) {
       res.end(im.encodeSync('png'));
     });
   });
+}
+
+// Ceres imagery tiles
+app.get('/imagery/:uuid/:z/:x/:y.png', function(req, res, next) {
+  generateTile(req, res, next, imagery({ uuid: req.params.uuid}));
+});
+
+// GSSURGO (Soil) tiles
+app.get('/soil/:z/:x/:y.png', function(req, res, next) {
+  generateTile(req, res, next, gssurgo);
 });
 
 // Start Server
