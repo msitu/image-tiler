cc5f9b0186329c38b6884369308e4ad796b91213
Mauricio Miranda <maurimiranda@gmail.com>
2019-08-01 18:35:19 -0300

    Add linter

--- download.js
+++ download.js
@@ -1,43 +1,41 @@
-const fs = require('fs');
-const http = require('http');
+const fs = require('fs')
+const http = require('http')
 
 // Download GeoTiff from S3 to the local cache
-module.exports = function(uuid) {
-  const path = `${process.env.CACHE_PATH}/${uuid}.tiff`;
-  const tmpPath = `${path}.tmp`;
+module.exports = function (uuid) {
+  const path = `${process.env.CACHE_PATH}/${uuid}.tiff`
+  const tmpPath = `${path}.tmp`
 
   return new Promise((resolve, reject) => {
-    const id = Date.now();
-
     if (fs.existsSync(path)) {
       // If file already exists, return
-      resolve(path);
+      resolve(path)
     } else if (fs.existsSync(tmpPath) &&
       (Date.now() - fs.statSync(tmpPath).birthtimeMs) < 30000) {
-        // If file is being downloaded, wait for it
-      const interval = setInterval(function () {
+      // If file is being downloaded, wait for it
+      const interval = setInterval(() => {
         if (fs.existsSync(path)) {
-          clearInterval(interval);
-          resolve(path);
+          clearInterval(interval)
+          resolve(path)
         }
-      }, 10);
+      }, 10)
     } else {
       // Else, download file
       const file = fs.createWriteStream(`${path}.tmp`)
         .on('error', reject)
-        .on('finish', function() {
-          file.close(function() {
-            fs.renameSync(tmpPath, path);
-            resolve(path);
-          });
-        });
+        .on('finish', () => {
+          file.close(() => {
+            fs.renameSync(tmpPath, path)
+            resolve(path)
+          })
+        })
 
-      http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, function(response) {
-        response.pipe(file);
+      http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, (response) => {
+        response.pipe(file)
       }).on('error', (error) => {
-        fs.unlinkSync(tmpPath);
-        reject(error);
-      });
+        fs.unlinkSync(tmpPath)
+        reject(error)
+      })
     }
   })
 }
--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-'use strict';
+const fs = require('fs')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const mapnik = require('mapnik')
+const SphericalMercator = require('@mapbox/sphericalmercator')
+const Handlebars = require('handlebars')
 
-const fs = require('fs');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const mapnik = require('mapnik');
-const SphericalMercator = require('@mapbox/sphericalmercator');
-const Handlebars = require('handlebars');
-
-const download = require('./download');
+const download = require('./download')
 
 // Load config from .env file
-require('dotenv').config();
+require('dotenv').config()
 
 // Register fonts and datasource plugins
-mapnik.register_default_fonts();
-mapnik.register_default_input_plugins();
+mapnik.register_default_fonts()
+mapnik.register_default_input_plugins()
 
-const mercator = new SphericalMercator();
+const mercator = new SphericalMercator()
 
-const uuidPattern = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i;
+const uuidPattern = /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i
 
 // Setup imagery config template
-const imagery = Handlebars.compile(fs.readFileSync('imagery.xml', 'utf8'));
+const imagery = Handlebars.compile(fs.readFileSync('imagery.xml', 'utf8'))
 
 // Setup gssurgo config
 const gssurgo = Handlebars.compile(fs.readFileSync('gssurgo.xml', 'utf8'))({
@@ -32,111 +30,109 @@ const gssurgo = Handlebars.compile(fs.readFileSync('gssurgo.xml', 'utf8'))({
   password: process.env.DB_PASS,
   dbname: process.env.DB_NAME,
   table: process.env.DB_TABLE
-});
+})
 
 // Create Express App
-const app = express();
+const app = express()
 
 // Add CORS
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true }))
 
 // Add logger
-app.use(morgan(':date[iso] :remote-addr :url :status :response-time ms'));
+app.use(morgan(':date[iso] :remote-addr :url :status :response-time ms'))
 
 // Generate PNG and respond the request with it
-const generateImage = function(map, res, next) {
+const generateImage = function (map, res, next) {
   // Create image object from map config
-  const image = new mapnik.Image(map.width, map.height);
+  const image = new mapnik.Image(map.width, map.height)
 
   // Render and return PNG
-  map.render(image, function(err, tile) {
-    if (err) return next(err);
+  map.render(image, (renderError, tile) => {
+    if (renderError) return next(renderError)
 
-    tile.encode('png', function(err, png) {
-      if (err) return next(err);
+    tile.encode('png', (encodeError, png) => {
+      if (encodeError) return next(encodeError)
 
       // Set cache age to 90 days
-      res.set('Cache-Control', 'max-age=7776000');
+      res.set('Cache-Control', 'max-age=7776000')
 
-      res.writeHead(200, { 'Content-Type': 'image/png' });
-      res.end(png);
+      res.writeHead(200, { 'Content-Type': 'image/png' })
+      res.end(png)
 
       // Call Garbage Collector to avoid memory issues
-      global.gc();
-    });
-  });
-};
+      global.gc()
+    })
+  })
+}
 
 // Generate tile base on config
-const generateTile = function(req, res, next, config) {
+function generateTile (req, res, next, config) {
   // Check XYZ parameters
-  if (isNaN(req.params.x) || isNaN(req.params.y) || isNaN(req.params.z)) {
-    res.status(422).send('Bad format: XYZ')
-    return
+  if (Number.isNan(req.params.x) || Number.isNan(req.params.y) || Number.isNan(req.params.z)) {
+    return res.status(422).send('Bad format: XYZ')
   }
 
   // Create Mapnik object
-  const map = new mapnik.Map(256, 256);
+  const map = new mapnik.Map(256, 256)
 
   // Read map config
-  map.fromString(config, function(err, map) {
-    if (err) return next(err);
+  map.fromString(config, (readError) => {
+    if (readError) return next(readError)
 
     // Convert XYZ to BoundingBox and set the Map extent
-    map.zoomToBox(mercator.bbox(req.params.x, req.params.y, req.params.z, false, '900913'));
+    map.zoomToBox(mercator.bbox(req.params.x, req.params.y, req.params.z, false, '900913'))
 
-    generateImage(map, res, next);
-  });
-};
+    generateImage(map, res, next)
+  })
+}
 
 // Ceres imagery tiles
-app.get('/imagery/:uuid/:z/:x/:y.png', function(req, res, next) {
+app.get('/imagery/:uuid/:z/:x/:y.png', (req, res, next) => {
   // Check UUID parameter
   if (!uuidPattern.test(req.params.uuid)) {
-    res.status(422).send('Bad format: UUID')
-    return
+    return res.status(422).send('Bad format: UUID')
   }
 
   download(req.params.uuid)
     .then((path) => {
-      generateTile(req, res, next, imagery({ path }));
+      generateTile(req, res, next, imagery({ path }))
     })
-    .catch(next);
-});
+    .catch(next)
+})
 
 // GSSURGO (Soil) tiles
-app.get('/soil/:z/:x/:y.png', function(req, res, next) {
-  generateTile(req, res, next, gssurgo);
-});
+app.get('/soil/:z/:x/:y.png', (req, res, next) => {
+  generateTile(req, res, next, gssurgo)
+})
 
 // Ceres imagery untiled image
-app.get('/imagery/:uuid.png', function(req, res, next) {
+app.get('/imagery/:uuid.png', (req, res, next) => {
   // Get size from query params
-  const width = req.query.width || 1024;
-  const height = req.query.height || 1024;
+  const width = req.query.width || 1024
+  const height = req.query.height || 1024
 
   // Create Mapnik object
-  const map = new mapnik.Map(width, height);
+  const map = new mapnik.Map(width, height)
 
   download(req.params.uuid)
     .then((path) => {
-      map.fromString(imagery({ uuid: req.params.uuid}), function(err, map) {
-        if (err) return next(err);
+      map.fromString(imagery({ path }), (readError) => {
+        if (readError) return next(readError)
 
         // Zoom to image extent
-        map.zoomAll();
+        map.zoomAll()
 
-        generateImage(map, res, next);
-      });
+        generateImage(map, res, next)
+      })
     })
-    .catch(next);
-});
+    .catch(next)
+})
 
 // Server status check
-app.get('/status', function(req, res, next) {
-  res.sendStatus(200);
-});
+app.get('/status', (req, res) => {
+  res.sendStatus(200)
+})
 
 // Start Server
-app.listen(process.env.PORT, process.env.HOST);
-console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
+app.listen(process.env.PORT, process.env.HOST)
+console.info(`Running on http://${process.env.HOST}:${process.env.PORT}`)
