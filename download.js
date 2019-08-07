73fe8cabddb2e12c1ffb05a912596ddc12af7996
Mauricio Miranda <maurimiranda@gmail.com>
2019-08-07 18:43:03 -0300

    Add global error handler to download process

--- download.js
+++ download.js
@@ -7,35 +7,39 @@ module.exports = function (uuid) {
   const tmpPath = `${path}.tmp`
 
   return new Promise((resolve, reject) => {
-    if (fs.existsSync(path)) {
-      // If file already exists, return
-      resolve(path)
-    } else if (fs.existsSync(tmpPath) &&
+    try {
+      if (fs.existsSync(path)) {
+        // If file already exists, return
+        resolve(path)
+      } else if (fs.existsSync(tmpPath) &&
       (Date.now() - fs.statSync(tmpPath).birthtimeMs) < 30000) {
-      // If file is being downloaded, wait for it
-      const interval = setInterval(() => {
-        if (fs.existsSync(path)) {
-          clearInterval(interval)
-          resolve(path)
-        }
-      }, 10)
-    } else {
-      // Else, download file
-      const file = fs.createWriteStream(`${path}.tmp`)
-        .on('error', reject)
-        .on('finish', () => {
-          file.close(() => {
-            fs.renameSync(tmpPath, path)
+        // If file is being downloaded, wait for it
+        const interval = setInterval(() => {
+          if (fs.existsSync(path)) {
+            clearInterval(interval)
             resolve(path)
+          }
+        }, 10)
+      } else {
+        // Else, download file
+        const file = fs.createWriteStream(`${path}.tmp`)
+          .on('error', reject)
+          .on('finish', () => {
+            file.close(() => {
+              fs.renameSync(tmpPath, path)
+              resolve(path)
+            })
           })
-        })
 
-      http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, (response) => {
-        response.pipe(file)
-      }).on('error', (error) => {
-        fs.unlinkSync(tmpPath)
-        reject(error)
-      })
+        http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, (response) => {
+          response.pipe(file)
+        }).on('error', (error) => {
+          fs.unlinkSync(tmpPath)
+          reject(error)
+        })
+      }
+    } catch (error) {
+      reject(error)
     }
   })
 }
