37b4aae2c9352515864ce57734fe91a91f0a473d
Mauricio Miranda <maurimiranda@gmail.com>
2019-08-08 14:42:59 -0300

    Improve error handler

--- download.js
+++ download.js
@@ -7,39 +7,40 @@ module.exports = function (uuid) {
   const tmpPath = `${path}.tmp`
 
   return new Promise((resolve, reject) => {
-    try {
-      if (fs.existsSync(path)) {
-        // If file already exists, return
-        resolve(path)
-      } else if (fs.existsSync(tmpPath) &&
-      (Date.now() - fs.statSync(tmpPath).birthtimeMs) < 30000) {
-        // If file is being downloaded, wait for it
-        const interval = setInterval(() => {
-          if (fs.existsSync(path)) {
-            clearInterval(interval)
+    if (fs.existsSync(path)) {
+      // If file already exists, return
+      resolve(path)
+    } else if (fs.existsSync(tmpPath) &&
+    (Date.now() - fs.statSync(tmpPath).birthtimeMs) < 30000) {
+      // If file is being downloaded, wait for it
+      const interval = setInterval(() => {
+        if (fs.existsSync(path)) {
+          clearInterval(interval)
+          resolve(path)
+        }
+      }, 10)
+    } else {
+      // Else, download file
+      const file = fs.createWriteStream(`${path}.tmp`)
+        .on('error', reject)
+        .on('finish', () => {
+          try {
+            fs.renameSync(tmpPath, path)
             resolve(path)
+          } catch (error) {
+            reject(error)
           }
-        }, 10)
-      } else {
-        // Else, download file
-        const file = fs.createWriteStream(`${path}.tmp`)
-          .on('error', reject)
-          .on('finish', () => {
-            file.close(() => {
-              fs.renameSync(tmpPath, path)
-              resolve(path)
-            })
-          })
-
-        http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, (response) => {
-          response.pipe(file)
-        }).on('error', (error) => {
-          fs.unlinkSync(tmpPath)
-          reject(error)
         })
-      }
-    } catch (error) {
-      reject(error)
+
+      http.get(`http://s3-us-west-2.amazonaws.com/ceres-geotiff-data/${uuid}.tif`, (response) => {
+        response.pipe(file)
+      }).on('error', (error) => {
+        try {
+          fs.unlinkSync(tmpPath)
+        } finally {
+          reject(error)
+        }
+      })
     }
   })
 }
