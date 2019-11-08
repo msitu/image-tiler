2539a9ac578ed7d6de1e567d59097beffbdb186e
Mauricio Miranda <maurimiranda@gmail.com>
2019-11-08 12:22:25 -0300

    Improve tests

--- test/run.sh
+++ test/run.sh
@@ -3,17 +3,33 @@
 export HOST=0.0.0.0
 export PORT=8888
 
-echo "Imagery Single Image"
-curl -s "http://$HOST:$PORT/imagery/7326e81d-40b0-4053-8f33-bd22f9a53df9.png" | cmp test/fixtures/imagery-image.png && echo "OK"
+test()
+{
+    # $1: Test title
+    # $2: Resource path
+    # $3: Fixture path
 
-echo "Combo Image"
-curl -s "http://$HOST:$PORT/combo/7326e81d-40b0-4053-8f33-bd22f9a53df9.png" | cmp test/fixtures/combo-image.png && echo "OK"
+    echo "============================="
+    echo $1
+    /usr/bin/time -f "%es" curl -s "http://$HOST:$PORT/$2" | cmp test/fixtures/$3 && echo "OK"
+}
 
-echo "Imagery Raster Tile"
-curl -s "http://$HOST:$PORT/imagery/7326e81d-40b0-4053-8f33-bd22f9a53df9/17/21455/50471.png" | cmp test/fixtures/imagery-raster-tile.png && echo "OK"
+test "Imagery Single Image" \
+    imagery/7326e81d-40b0-4053-8f33-bd22f9a53df9.png \
+    imagery-image.png
 
-echo "Soil Raster Tile"
-curl -s "http://$HOST:$PORT/soil/14/3364/6683.png" | cmp test/fixtures/soil-image-tile.png && echo "OK"
+test "Imagery Raster Tile" \
+    imagery/7326e81d-40b0-4053-8f33-bd22f9a53df9/17/21455/50471.png \
+    imagery-raster-tile.png
 
-echo "Soil Vector Tile"
-curl -s "http://$HOST:$PORT/soil/14/3364/6683.mvt" | cmp test/fixtures/soil-vector-tile.mvt && echo "OK"
+test "Combo Image" \
+    combo/7326e81d-40b0-4053-8f33-bd22f9a53df9.png \
+    combo-image.png
+
+test "Soil Raster Tile" \
+    soil/14/3364/6683.png \
+    soil-image-tile.png
+
+test "Soil Vector Tile" \
+    soil/14/3364/6683.mvt \
+    soil-vector-tile.mvt
