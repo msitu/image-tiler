da673946674490e9ef4e1a32f7f21e2c344571ea
Mauricio Miranda <maurimiranda@gmail.com>
2020-01-28 15:44:34 -0300

    Add satellite source options. Use WMTS.

--- middlewares/satellite.js
+++ middlewares/satellite.js
@@ -1,6 +1,5 @@
 import mapnik from 'mapnik'
 import fs from 'fs'
-import json2xml from 'json2xml'
 
 // Load Mapnik datasource
 mapnik.registerDatasource(`${mapnik.settings.paths.input_plugins}/gdal.input`)
@@ -8,31 +7,71 @@ mapnik.registerDatasource(`${mapnik.settings.paths.input_plugins}/gdal.input`)
 // Read stylesheet files
 const satelliteStyle = fs.readFileSync('styles/satellite.xml', 'utf8')
 
-const baseConfig = {
-  GDAL_WMS: {
-    attr: { name: 'WMS' },
-    Service: {
-      ServerUrl: `${process.env.PROXY_BASE_URL}/wms`,
-      Layers: 'satellite',
-      CRS: 'EPSG:3857',
-      Version: '1.3.0',
-      ImageFormat: 'image/png'
-    },
-    DataWindow: {
-      UpperLeftX: null,
-      UpperLeftY: null,
-      LowerRightX: null,
-      LowerRightY: null,
-      TileLevel: 20,
-      TileCountX: 1,
-      TileCountY: 1
-    },
-    BandsCount: 3,
-    Cache: {
-      Path: process.env.CACHE_PATH
-    }
-  }
+const config = {
+  wms: `<GDAL_WMS>
+          <Service name="WMS">
+            <ServerUrl>${process.env.PROXY_BASE_URL}/wms</ServerUrl>
+            <Layers>satellite</Layers>
+            <CRS>EPSG:3857</CRS>
+            <Version>1.3.0</Version>
+            <ImageFormat>image/png</ImageFormat>
+          </Service>
+          <DataWindow>
+            <UpperLeftX>-20037508.34278924</UpperLeftX>
+            <UpperLeftY>20037508.34278924</UpperLeftY>
+            <LowerRightX>20037508.34278924</LowerRightX>
+            <LowerRightY>-20037508.34278924</LowerRightY>
+            <TileLevel>20</TileLevel>
+            <TileCountX>1</TileCountX>
+            <TileCountY>1</TileCountY>
+          </DataWindow>
+          <Cache>
+            <Path>${process.env.CACHE_PATH}</Path>
+          </Cache>
+        </GDAL_WMS>`,
+  tms: `<GDAL_WMS>
+          <Service name="TMS">
+            <ServerUrl>${process.env.PROXY_BASE_URL}/tiles/satellite/EPSG3857/\${z}/\${x}/\${y}.png</ServerUrl>
+          </Service>
+          <DataWindow>
+            <UpperLeftX>-20037508.34278924</UpperLeftX>
+            <UpperLeftY>20037508.34278924</UpperLeftY>
+            <LowerRightX>20037508.34278924</LowerRightX>
+            <LowerRightY>-20037508.34278924</LowerRightY>
+            <TileLevel>16</TileLevel>
+            <TileCountX>1</TileCountX>
+            <TileCountY>1</TileCountY>
+            <YOrigin>bottom</YOrigin>
+          </DataWindow>
+          <BlockSizeX>256</BlockSizeX>
+          <BlockSizeY>256</BlockSizeY>
+          <Projection>EPSG:3857</Projection>
+          <Cache>
+            <Path>${process.env.CACHE_PATH}</Path>
+          </Cache>
+        </GDAL_WMS>`,
+  wmts: `<GDAL_WMTS>
+          <GetCapabilitiesUrl>${process.env.PROXY_BASE_URL}/service?REQUEST=GetCapabilities&amp;SERVICE=WMTS</GetCapabilitiesUrl>
+          <Layer>satellite</Layer>
+          <TileMatrixSet>webmercator</TileMatrixSet>
+          <Style>default</Style>
+          <Format>image/png</Format>
+          <DataWindow>
+            <UpperLeftX>-20037508.34278924</UpperLeftX>
+            <UpperLeftY>20037508.34278924</UpperLeftY>
+            <LowerRightX>20037508.34278924</LowerRightX>
+            <LowerRightY>-20037508.34278924</LowerRightY>
+          </DataWindow>
+          <BandsCount>4</BandsCount>
+          <Cache>
+            <Path>${process.env.CACHE_PATH}</Path>
+          </Cache>
+        </GDAL_WMTS>`
 }
+
+// Write config file
+const filePath = `${process.env.CACHE_PATH}/satellite.xml`
+fs.writeFileSync(filePath, config.wmts)
 
 export const satelliteLayer = (req, res, next) => {
   const { buffer = 0.25 } = req.query
@@ -41,21 +80,6 @@ export const satelliteLayer = (req, res, next) => {
   map.fromStringSync(satelliteStyle)
 
   map.bufferSize = map.width * buffer
-
-  // Write config file
-  const filePath = `${process.env.CACHE_PATH}/${map.bufferedExtent.join('_')}.xml`
-
-  if (!fs.existsSync(filePath)) {
-    // Define specific satellite extent using imagery bounds + buffer
-    // This adds some area context to the image
-    const config = { ...baseConfig }
-    config.GDAL_WMS.DataWindow.UpperLeftX = map.bufferedExtent[0]
-    config.GDAL_WMS.DataWindow.UpperLeftY = map.bufferedExtent[1]
-    config.GDAL_WMS.DataWindow.LowerRightX = map.bufferedExtent[2]
-    config.GDAL_WMS.DataWindow.LowerRightY = map.bufferedExtent[3]
-
-    fs.writeFileSync(filePath, json2xml(config, { attributes_key: 'attr' }))
-  }
 
   // Create satellite layer
   const satelliteLayer = new mapnik.Layer('satellite', '+init=epsg:3857')
@@ -67,8 +91,8 @@ export const satelliteLayer = (req, res, next) => {
 
   map.add_layer(satelliteLayer)
 
-  // Zoom to GeoTiff + Buffer
-  map.zoomAll()
+  // Zoom to current extent + buffer
+  map.zoomToBox(map.bufferedExtent)
 
   next()
 }
