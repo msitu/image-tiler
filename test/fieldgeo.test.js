8840848974050e43be8ce174b9f1cca7b39be602
Mauricio Miranda <maurimiranda@gmail.com>
2020-04-08 19:27:55 -0300

    Update fieldgeo fixtures

--- test/fieldgeo.test.js
+++ test/fieldgeo.test.js
@@ -7,22 +7,22 @@ jest.setTimeout(60000);
 const request = supertest(app);
 const fixture = fs.readFileSync;
 const base = 'fieldgeo';
-const farm = 'a91d395b-79ae-44f7-981e-0f8dea1f1074';
-const field = 'fbd0968a-912b-47f0-924c-ef40ed350e72';
+const farm = '7355293c-e23d-4aab-8ff0-e2f8f1b83f4e';
+const field = 'e6437d6f-4637-4133-bb17-9da0eff0b963';
 
 describe('fieldgeo routes', () => {
   test('should return a vector tile filter by farm', async done => {
-    const res = await request.get(`/${base}/${farm}/13/1299/3139.mvt`).responseType('arraybuffer');
+    const res = await request.get(`/${base}/${farm}/14/3534/6442.mvt`).responseType('arraybuffer');
 
-    expect(res.body.equals(fixture('test/fixtures/field-farm-vector-tile.mvt'))).toBeTruthy();
+    expect(res.body.equals(fixture('test/fixtures/fieldgeo-farm-vector-tile.mvt'))).toBeTruthy();
 
     done();
   });
 
   test('should return a vector tile filter by field', async done => {
-    const res = await request.get(`/${base}/${farm}/${field}/17/20789/50226.mvt`).responseType('arraybuffer');
+    const res = await request.get(`/${base}/${farm}/${field}/15/7068/12884.mvt`).responseType('arraybuffer');
 
-    expect(res.body.equals(fixture('test/fixtures/field-field-vector-tile.mvt'))).toBeTruthy();
+    expect(res.body.equals(fixture('test/fixtures/fieldgeo-field-vector-tile.mvt'))).toBeTruthy();
 
     done();
   });
