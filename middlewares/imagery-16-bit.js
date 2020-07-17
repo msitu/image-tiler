1860889817ec8402c6297bc646b10e9b27dc0366
msitu@ceresimaging.net <60153807+msitu@users.noreply.github.com>
2020-07-17 14:53:13 -0700

    cleanup calculateCenter code

--- middlewares/imagery-16-bit.js
+++ middlewares/imagery-16-bit.js
@@ -12,22 +12,28 @@ export const imageryLayer16Bit = (req, res, next) => {
 
   map.fromStringSync(style);
 
+  // TODO: investigate what valid values for `band` are
   // Create layer based on imagery Geotiff file
   const layer = new mapnik.Layer('imagery');
-  layer.datasource = new mapnik.Datasource({
+  const datasource = new mapnik.Datasource({
     type: 'gdal',
     band: 3,
     file: path
   });
-  
-  const [minx, miny, maxx, maxy]  = layer.datasource.extent()
-  const lat = (miny + maxy)/2
-  const lon = (minx + maxx)/2
-  res.locals.center = [lat, lon]
-
+  layer.datasource = datasource
   layer.styles = ['imagery'];
 
   map.add_layer(layer);
 
+  res.locals.center = calculateCenter(datasource)
+
   next();
 };
+
+// Calculate the center point from a GeoTiff Mapnik datasource
+const calculateCenter = (datasource) => {
+  const [minx, miny, maxx, maxy]  = datasource.extent()
+  const lat = (miny + maxy)/2
+  const lon = (minx + maxx)/2
+  return [lat, lon]
+}
